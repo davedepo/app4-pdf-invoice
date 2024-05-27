cb90719afbffb4ef3e9fe5c49b317c3985840a90
Pratik <pratik.dave@msn.com>
2024-05-27 22:58:36 +0530

    D25: Create minimal PDFs for each excel

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import glob
+from fpdf import FPDF
+from pathlib import Path
 
 # to create python list that contains all filepaths
 filepaths = glob.glob("invoices/*.xlsx")
@@ -8,7 +10,12 @@ print(filepaths)
 # to read each file we initiate for-loop
 # we will get dependency warning - Missing optional dependency 'openpyxl'
 # install openpyxl from python packages or terminal
-# out of code will create df - stores tabular data of all xlsx files
 for filepath in filepaths:
 	df = pd.read_excel(filepath, sheet_name="Sheet 1")
-	print(df)
+	pdf = FPDF(orientation="P", unit="mm", format="A4")
+	pdf.add_page()
+	filename = Path(filepath).stem
+	invoice_nr = filename.split("-")[0]
+	pdf.set_font(family="Times", size=16, style="B")
+	pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}")
+	pdf.output(f"PDFs/{filename}.pdf")
