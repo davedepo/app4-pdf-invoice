80c822aaf1d3a2754b5a03b01e6cdebb4d352057
Pratik <pratik.dave@msn.com>
2024-05-28 20:44:09 +0530

    D26: Exercise - Add content from multiple .txt files

--- txt_to_pdf.py
+++ txt_to_pdf.py
@@ -9,10 +9,26 @@ print(filepaths)
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 for filepath in filepaths:
+	# to add blank pg in PDF for each txt file
 	pdf.add_page()
-	filename = Path(filepath).stem
-	print(filename)
-	pdf.set_font(family="Times", style="B", size=16)
-	pdf.cell(w=50, h=8, txt=f"{filename.capitalize()}")
 
-pdf.output(f"PDFs/txt_to_pdf.pdf")
+	# to extract filename from pathlib for each txt file
+	filename = Path(filepath).stem
+	# print(filename) test output
+
+	# to create headers from each .txt filename
+	pdf.set_font(family="Times", style="B", size=16)
+	pdf.cell(w=50, h=8, txt=f"{filename.capitalize()}", ln=1)
+	pdf.line(10, 20, 200, 20)
+	pdf.cell(w=50, h=4, txt="", ln=1)
+
+	# to open and read .txt files from filepath
+	with open(filepath, "r") as file:
+		content = file.read()
+
+	# to set pg font, execute multi-cell output from each .txt
+	pdf.set_font(family="Times", size=12)
+	pdf.multi_cell(w=0, h=6, txt=content)
+
+# creat 1 single PDF output from multiple txt file (Out-side loop)
+pdf.output("PDFs/txt_to_pdf.pdf")
