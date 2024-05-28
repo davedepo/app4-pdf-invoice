2bc5d8d1de60639fe115506840f7be77617d96b2
Pratik <pratik.dave@msn.com>
2024-05-28 18:53:04 +0530

    D26: Add tables

--- xlsx_to_pdf.py
+++ xlsx_to_pdf.py
@@ -11,7 +11,6 @@ print(filepaths)
 # we will get dependency warning - Missing optional dependency 'openpyxl'
 # install openpyxl from python packages or terminal
 for filepath in filepaths:
-	df = pd.read_excel(filepath, sheet_name="Sheet 1")
 
 	pdf = FPDF(orientation="P", unit="mm", format="A4")
 	pdf.add_page()
@@ -23,6 +22,29 @@ for filepath in filepaths:
 	pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}", ln=1)
 
 	pdf.set_font(family="Times", size=16, style="B")
-	pdf.cell(w=50, h=8, txt=f"Date: {date}")
+	pdf.cell(w=50, h=8, txt=f"Date: {date}", ln=1)
+
+	df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
+	columns_to_list = df.columns
+	columns_to_list = [item.replace("_", " ").title()
+	                   for item in columns_to_list]
+
+	pdf.set_font(family="Times", size=10, style="B")
+	pdf.set_text_color(80, 80, 80)
+	pdf.cell(w=30, h=8, txt=columns_to_list[0], border=1)
+	pdf.cell(w=70, h=8, txt=columns_to_list[1], border=1)
+	pdf.cell(w=35, h=8, txt=columns_to_list[2], border=1)
+	pdf.cell(w=30, h=8, txt=columns_to_list[3], border=1)
+	pdf.cell(w=30, h=8, txt=columns_to_list[4], border=1, ln=1)
+
+	for index, row in df.iterrows():
+		pdf.set_font(family="Times", size=10)
+		pdf.set_text_color(80, 80, 80)
+		pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+		pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
+		pdf.cell(w=35, h=8, txt=str(row["amount_purchased"]), border=1)
+		pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+		pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
 	pdf.output(f"PDFs/{filename}.pdf")
